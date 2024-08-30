3d6c83381f23db9ec8f88c49e8d031173a514deb
CristianoCarta <cristiano.carta23@gmail.com>
2024-08-30 16:26:22 +0200

    Benchmark done updated to f3

--- benchmark.py
+++ benchmark.py
@@ -13,6 +13,7 @@ d = [(3, 32, 32), (3, 64, 64), (3, 128, 128),
  (3, 256, 256), (3, 512, 512), (3, 640, 640), (3, 800, 800), (3, 1024, 1024)]
 D = 5
 iterations = 100
+selected_label = 6
 
 benchmark = F1()
 
@@ -31,16 +32,16 @@ for item in N:
 for item in N:
     benchmark.create_dataset(item,f"outputs/f2/row_wise/sec2/f2_{item}","sec2")
 
-t1,t2,t3,t4 = benchmark.benchmark_row_wise("outputs/f2/row_wise/core/f2",N,iterations,6,hdf5_driver="core",plot=True,save=True)
-t1,t2,t3,t4 = benchmark.benchmark_row_wise("outputs/f2/row_wise/sec2/f2",N,iterations,6,hdf5_driver="sec2",plot=True,save=True)
+t1,t2,t3,t4 = benchmark.benchmark_row_wise("outputs/f2/row_wise/core/f2",N,iterations,selected_label,hdf5_driver="core",plot=True,save=True)
+t1,t2,t3,t4 = benchmark.benchmark_row_wise("outputs/f2/row_wise/sec2/f2",N,iterations,selected_label,hdf5_driver="sec2",plot=True,save=True)
 
 for item in N:
     benchmark.create_dataset(item,f"outputs/f2/bounding_boxes/core/f2_{item}","core")
 for item in N:
     benchmark.create_dataset(item,f"outputs/f2/bounding_boxes/sec2/f2_{item}","sec2")
 
-t1,t2,t3,t4 = benchmark.benchmark_bounding_boxes_conversion("outputs/f2/bounding_boxes/core/f2",N,iterations,6,hdf5_driver="core",plot=True,save=True)
-t1,t2,t3,t4 = benchmark.benchmark_bounding_boxes_conversion("outputs/f2/bounding_boxes/sec2/f2",N,iterations,6,hdf5_driver="sec2",plot=True,save=True)
+t1,t2,t3,t4 = benchmark.benchmark_bounding_boxes_conversion("outputs/f2/bounding_boxes/core/f2",N,iterations,selected_label,hdf5_driver="core",plot=True,save=True)
+t1,t2,t3,t4 = benchmark.benchmark_bounding_boxes_conversion("outputs/f2/bounding_boxes/sec2/f2",N,iterations,selected_label,hdf5_driver="sec2",plot=True,save=True)
 
 benchmark = F3()
 
@@ -53,3 +54,41 @@ for item in N:
 
 t1,t2,t3,t4 = benchmark.benchmark_heatmap_column_wise("outputs/f3/column_wise/core/f3",N,d,D,iterations,hdf5_driver="core",plot=True,save=True)
 t1,t2,t3,t4 = benchmark.benchmark_heatmap_column_wise("outputs/f3/column_wise/sec2/f3",N,d,D,iterations,hdf5_driver="sec2",plot=True,save=True)
+
+selected_label = 1
+
+for item in N:
+    for res in tqdm(d):
+        benchmark.create_dataset(item,res,D,f"outputs/f3/row_wise/core/f3_{item}_{res}_{D}","core")
+for item in N:
+    for res in tqdm(d):
+        benchmark.create_dataset(item,res,D,f"outputs/f3/row_wise/sec2/f3_{item}_{res}_{D}","sec2")
+
+t1,t2,t3,t4 = benchmark.benchmark_heatmap_row_wise("outputs/f3/row_wise/core/f3",N,d,D,iterations,selected_label,hdf5_driver="core",plot=True,save=True)
+t1,t2,t3,t4 = benchmark.benchmark_heatmap_row_wise("outputs/f3/row_wise/sec2/f3",N,d,D,iterations,selected_label,hdf5_driver="sec2",plot=True,save=True)
+
+
+D = 10
+
+
+for item in N:
+    for res in tqdm(d):
+        benchmark.create_dataset(item,res,D,f"outputs/f3/column_wise/core/f3_{item}_{res}_{D}","core")
+for item in N:
+    for res in tqdm(d):
+        benchmark.create_dataset(item,res,D,f"outputs/f3/column_wise/sec2/f3_{item}_{res}_{D}","sec2")
+
+t1,t2,t3,t4 = benchmark.benchmark_heatmap_column_wise("outputs/f3/column_wise/core/f3",N,d,D,iterations,hdf5_driver="core",plot=True,save=True)
+t1,t2,t3,t4 = benchmark.benchmark_heatmap_column_wise("outputs/f3/column_wise/sec2/f3",N,d,D,iterations,hdf5_driver="sec2",plot=True,save=True)
+
+selected_label = 1
+
+for item in N:
+    for res in tqdm(d):
+        benchmark.create_dataset(item,res,D,f"outputs/f3/row_wise/core/f3_{item}_{res}_{D}","core")
+for item in N:
+    for res in tqdm(d):
+        benchmark.create_dataset(item,res,D,f"outputs/f3/row_wise/sec2/f3_{item}_{res}_{D}","sec2")
+
+t1,t2,t3,t4 = benchmark.benchmark_heatmap_row_wise("outputs/f3/row_wise/core/f3",N,d,D,iterations,selected_label,hdf5_driver="core",plot=True,save=True)
+t1,t2,t3,t4 = benchmark.benchmark_heatmap_row_wise("outputs/f3/row_wise/sec2/f3",N,d,D,iterations,selected_label,hdf5_driver="sec2",plot=True,save=True)
--- src/benchmarkers/f3.py
+++ src/benchmarkers/f3.py
@@ -243,4 +243,197 @@ class F3(SampleGenerator):
             np.save(f"{path}_t_manipulate_hdf5_{hdf5_driver}_{D}.npy",t_manipulate_hdf5)
             
         return t_load_arrow, t_manipulate_arrow , t_load_hdf5 , t_manipulate_hdf5
+    
+
+    def benchmark_heatmap_row_wise(self,
+                  path : str, 
+                  N : List[int],
+                  d : List[Tuple],
+                  D : int,
+                  iterations : int,
+                  selected_label : int,
+                  hdf5_driver : str = None,
+                  plot : bool = False,
+                  save : bool = False):
+         
+        t_load_hdf5 = []
+        t_manipulate_hdf5 = []
+
+        t_load_arrow = []
+        t_manipulate_arrow = []
+        
+
+        for item in N:
+
+            tmp_load_res_arrow = []
+            tmp_manipulate_res_arrow = []
+
+            tmp_load_res_hdf5 = []
+            tmp_manipulate_res_hdf5 = []
+
+            for res in d:
+                tmp_load_arrow = []
+                tmp_manipulate_arrow = []
+
+                tmp_load_hdf5 = []
+                tmp_manipulate_hdf5 = []
+
+                for j in tqdm(range(iterations)):
+                    ### LOADING ###
+                    st_time_arrow = time.time()
+                    table = pq.read_table(f'{path}_{item}_{res}_{D}.parquet')
+                    en_time_arrow = time.time()
+                    tmp_load_arrow.append(en_time_arrow - st_time_arrow)
+                    
+                    ### MANIPULATION ###
+                    start_time_arrow = time.time()
+                    manipulated_images = []
+                    for k in range(item):
+                        if any( x == selected_label for x in table.column("image_feature").chunk(0)[k][0]["class_feature"].values.field("label").to_pylist()):  ## Indice di immagine deve variare nel framework
+                            obj = table.column("image_feature").chunk(0)[k][0]["image"]
+                            image_numpy = np.frombuffer(obj.as_buffer(), dtype=np.float64).reshape(res)
+                            new_obj = np.transpose(image_numpy)   
+                            new_obj = np.square(new_obj)
+                            new_obj = np.exp(new_obj)
+                            new_obj = np.transpose(new_obj)
+                            manipulated_images.append(new_obj)
+                    end_time_arrow = time.time()
+                    tmp_manipulate_arrow.append(end_time_arrow-start_time_arrow)
+
+                    ### LOADING ###
+                    st_time_hdf5 = time.time()
+                    with h5py.File(f'{path}_{item}_{res}_{D}_{hdf5_driver}.h5', 'r', driver="core") as f:
+                        en_time_hdf5 = time.time()
+                        tmp_load_hdf5.append(en_time_hdf5 - st_time_hdf5)
+
+                        
+                        ### MANIPULATION ###
+                        def get_all_image_datasets():
+                            image_datasets = []                
+                            def visit_func(name, obj):
+                                if isinstance(obj, h5py.Dataset) and obj.attrs["type"] == "image" and obj.attrs["label"] == selected_label :
+                                    im_obj = obj[:]
+                                    new_obj = np.transpose(im_obj)   
+                                    new_obj = np.square(new_obj)
+                                    new_obj = np.exp(new_obj)
+                                    new_obj = np.transpose(new_obj)
+                                    image_datasets.append(new_obj)
+                            f.visititems(visit_func)
+                            return image_datasets
+
+                        start_time_hdf5 = time.time()
+                        image_ds = get_all_image_datasets()
+                        end_time_hdf5 = time.time()
+                        tmp_manipulate_hdf5.append(end_time_hdf5-start_time_hdf5)
+                
+                tmp_load_res_arrow.append(sum(tmp_load_arrow) / len(tmp_load_arrow))
+                tmp_manipulate_res_arrow.append(sum(tmp_manipulate_arrow) / len(tmp_manipulate_arrow))
+
+                tmp_load_res_hdf5.append(sum(tmp_load_hdf5) / len(tmp_load_hdf5))
+                tmp_manipulate_res_hdf5.append(sum(tmp_manipulate_hdf5) / len(tmp_manipulate_hdf5))
+                    
+            t_load_arrow.append(tmp_load_res_arrow)
+            t_manipulate_arrow.append(tmp_manipulate_res_arrow)
+
+            t_load_hdf5.append(tmp_load_res_hdf5)
+            t_manipulate_hdf5.append(tmp_manipulate_res_hdf5)
+
+        if plot:
+            data1 = np.array(t_load_arrow)
+            data2 = np.array(t_load_hdf5)
+            data3 = np.array(np.array(t_load_hdf5)/np.array(t_load_arrow))
+
+            N_label = [str(item) for item in N]
+            d_label = [str(item) for item in d]
+
+            fig, ax = plt.subplots(1, 3, figsize=(14, 6))
+            plt.suptitle("Loading benchmark")
+
+            vmin = min(data1.min(), data2.min())
+            vmax = max(data1.max(), data2.max())
+            norm = mcolors.Normalize(vmin=vmin, vmax=vmax)
+
+            # First heatmap
+            cax1 = ax[0].imshow(data1, cmap='coolwarm', interpolation='gaussian', norm=norm)
+            ax[0].set_title('HDF5')
+            fig.colorbar(cax1, ax=ax[0], orientation='vertical')
+            ax[0].set_xticks(ticks=np.arange(len(d_label)), labels=d_label,rotation=45)
+            ax[0].set_yticks(ticks=np.arange(len(N_label)), labels=N_label)
+
+            # Second heatmap
+            cax2 = ax[1].imshow(data2, cmap='coolwarm', interpolation='gaussian',norm=norm)
+            ax[1].set_title('Arrow')
+            fig.colorbar(cax2, ax=ax[1], orientation='vertical')
+            ax[1].set_xticks(ticks=np.arange(len(d_label)), labels=d_label,rotation=45)
+            ax[1].set_yticks(ticks=np.arange(len(N_label)), labels=N_label)
+
+            # Third heatmap
+            cax3 = ax[2].imshow(data3, cmap='inferno', interpolation='gaussian')
+            ax[2].set_title('HDF5 / Arrrow')
+            fig.colorbar(cax3, ax=ax[2], orientation='vertical')
+            ax[2].set_xticks(ticks=np.arange(len(d_label)), labels=d_label,rotation=45)
+            ax[2].set_yticks(ticks=np.arange(len(N_label)), labels=N_label)
+
+            # Adjust layout to prevent overlap
+            plt.tight_layout()
+
+            # Show the plot
+            plt.savefig(f'{path}_{hdf5_driver}_{D}_loading.pdf')  # Save as PDF
+            plt.show()
+
+##################################################################################################
+##################################################################################################
+##################################################################################################
+
+            data1 = np.array(t_manipulate_arrow)
+            data2 = np.array(t_manipulate_hdf5)
+            data3 = np.array(np.array(t_manipulate_hdf5)/np.array(t_manipulate_arrow))
+
+            N_label = [str(item) for item in N]
+            d_label = [str(item) for item in d]
+
+            fig, ax = plt.subplots(1, 3, figsize=(14, 6))
+            plt.suptitle("Manipulating benchmark")
+
+            vmin = min(data1.min(), data2.min())
+            vmax = max(data1.max(), data2.max())
+            norm = mcolors.Normalize(vmin=vmin, vmax=vmax)
+
+            # First heatmap
+            cax1 = ax[0].imshow(data1, cmap='coolwarm', interpolation='gaussian', norm=norm)
+            ax[0].set_title('HDF5')
+            fig.colorbar(cax1, ax=ax[0], orientation='vertical')
+            ax[0].set_xticks(ticks=np.arange(len(d_label)), labels=d_label,rotation=45)
+            ax[0].set_yticks(ticks=np.arange(len(N_label)), labels=N_label)
+
+            # Second heatmap
+            cax2 = ax[1].imshow(data2, cmap='coolwarm', interpolation='gaussian',norm=norm)
+            ax[1].set_title('Arrow')
+            fig.colorbar(cax2, ax=ax[1], orientation='vertical')
+            ax[1].set_xticks(ticks=np.arange(len(d_label)), labels=d_label,rotation=45)
+            ax[1].set_yticks(ticks=np.arange(len(N_label)), labels=N_label)
+
+            # Third heatmap
+            cax3 = ax[2].imshow(data3, cmap='inferno', interpolation='gaussian')
+            ax[2].set_title('HDF5 / Arrrow')
+            fig.colorbar(cax3, ax=ax[2], orientation='vertical')
+            ax[2].set_xticks(ticks=np.arange(len(d_label)), labels=d_label,rotation=45)
+            ax[2].set_yticks(ticks=np.arange(len(N_label)), labels=N_label)
+
+            # Adjust layout to prevent overlap
+            plt.tight_layout()
+            
+
+            # Show the plot
+            plt.savefig(f'{path}_{hdf5_driver}_{D}_manipulating.pdf')  # Save as PDF
+            plt.show()
+
+
+        if save:
+            np.save(f"{path}_t_load_arrow_{D}.npy",t_load_arrow)
+            np.save(f"{path}_t_manipulate_arrow_{D}.npy",t_manipulate_arrow)
+            np.save(f"{path}_t_load_hdf5_{hdf5_driver}_{D}.npy",t_load_hdf5)
+            np.save(f"{path}_t_manipulate_hdf5_{hdf5_driver}_{D}.npy",t_manipulate_hdf5)
+            
+        return t_load_arrow, t_manipulate_arrow , t_load_hdf5 , t_manipulate_hdf5
 
