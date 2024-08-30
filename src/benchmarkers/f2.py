9448a672117b97d0dec5e52d49da0a21c51032b1
CristianoCarta <cristiano.carta23@gmail.com>
2024-08-30 16:08:32 +0200

    panic commit

--- src/benchmarkers/f2.py
+++ src/benchmarkers/f2.py
@@ -4,6 +4,10 @@ import pyarrow as pa
 import pyarrow.parquet as pq
 import h5py
 import random
+from typing import List
+import time
+from tqdm import tqdm
+from matplotlib import pyplot as plt
 
 # FORMATS
 # 0 = YOLO
@@ -21,15 +25,34 @@ def generate_phrase():
     verb = random.choice(verbs)
     return f"The {adj} {noun} {verb}"
 
+def xyminmax_to_xycenterwh(bbox : List):
+    if bbox[4] == 1:  ## ONLY IF XY IS FOUND
+        width = bbox[2] - bbox[0]
+        height = bbox[3] - bbox[1]
+        x_center = bbox[0] + width / 2
+        y_center = bbox[1] + height / 2
+        return x_center, y_center, width, height
+    else:
+        return [bbox[0],bbox[1],bbox[2],bbox[3]]
+
+def xycenterwh_to_xyminmax(bbox : List):
+    if bbox[4] == 0:  ## ONLY IF YOLO IS FOUND
+        x_min = bbox[0] - bbox[2] / 2
+        y_min = bbox[1] - bbox[3] / 2
+        x_max = bbox[0] + bbox[2] / 2
+        y_max = bbox[1] + bbox[3] / 2
+        return [x_min, y_min, x_max, y_max]
+    else:
+        return [bbox[0],bbox[1],bbox[2],bbox[3]]
 class F2(SampleGenerator):
 
     def __init__(self):
         self.texts = [generate_phrase() for i in range(10)]
 
 
-    def create_dataset(self, N: int, name : str = "data_hierarchy"):
+    def create_dataset(self, N: int, name : str , hdf5_driver : str):
         output = []
-        with h5py.File(name+".h5", 'w', driver="core") as f:
+        with h5py.File(f"{name}_{hdf5_driver}.h5", 'w', driver="core") as f:
             for i in range(N):
                 text = self.texts[int(random.randint(0,9))]
                 bb_class = np.random.randint(0,9)
@@ -82,12 +105,11 @@ class F2(SampleGenerator):
 
                 image_feature = example.create_group('image_feature')
                 image = image_feature.create_group('image')
-                image.create_dataset('image', data=im)  
+                image_ds = image.create_dataset('image', data=im)
+                image_ds.attrs["type"] = "image"
+                image_ds.attrs["label"] = im_class  
 
-                class_feature1 = image.create_group("class_feature")
-                class1 = class_feature1.create_group("class_image")
-                class1.create_dataset("label",data=im_class)
-
+            
                 boundingbox_feature1 = image.create_group('boundingbox_feature')
                 bb1_g = boundingbox_feature1.create_group('bb1')
                 bb2_g = boundingbox_feature1.create_group('bb2')
@@ -95,18 +117,251 @@ class F2(SampleGenerator):
                 bb1_d.attrs["format"] = 0
                 bb2_d = bb2_g.create_dataset('bbox', data=bb2)
                 bb2_d.attrs["format"] = 1
-                #bb1_g.create_dataset('format', data=0)  
-                #bb2_g.create_dataset('format', data=1)
+                bb2_d.attrs["label"] = bb_class
+                bb2_d.attrs["type"] = "bbox"
+                
 
                 text_feature = bb1_g.create_group("text_feature")
                 text_feature.create_dataset("description",data=text)
 
-                class_feature2 = bb2_g.create_group("class_feature")
-                class_feature2.create_dataset("label",data=bb_class)
         
         table = pa.Table.from_pylist(output)
         df = table.to_pandas()
         df.to_parquet(name+".parquet")
+
+    def benchmark_row_wise(self,
+                  path : str, 
+                  N : List[int],
+                  iterations : int,
+                  selected_label : int,
+                  hdf5_driver : str = None,
+                  plot : bool = False,
+                  save : bool = False):
+        
+        t_load_hdf5 = []
+        t_manipulate_hdf5 = []
+
+        t_load_arrow = []
+        t_manipulate_arrow = []
+
+        for item in N:
+
+            tmp_load_hdf5 = []
+            tmp_manipulate_hdf5 = []
+
+            tmp_load_arrow = []
+            tmp_manipulate_arrow = []
+
+            
+            for j in tqdm(range(iterations)):
+                ### LOADING ###
+                st_time_arrow = time.time()
+                table = pq.read_table(f'{path}_{item}.parquet')
+                en_time_arrow = time.time()
+                tmp_load_arrow.append(en_time_arrow - st_time_arrow)
+
+                ### MANIPULATION ###
+                start_time_arrow = time.time()
+                
+                manipulated_images = []
+                for k in range(item):
+                    if any( x == selected_label for x in table.column("image_feature").chunk(0)[k][0]["class_feature"].values.field("label").to_pylist()):  ## Indice di immagine deve variare nel framework
+                        obj = table.column("image_feature").chunk(0)[k][0]["image"]
+                        image_numpy = np.frombuffer(obj.as_buffer(), dtype=np.float64).reshape(3, 125, 125)
+                        new_obj = np.transpose(image_numpy)   
+                        new_obj = np.square(new_obj)
+                        new_obj = np.exp(new_obj)
+                        new_obj = np.transpose(new_obj)
+                        manipulated_images.append(new_obj)
+                end_time_arrow = time.time()
+                tmp_manipulate_arrow.append(end_time_arrow-start_time_arrow)
+
+                ### LOADING ###
+                st_time_hdf5 = time.time()
+                with h5py.File(f'{path}_{item}_{hdf5_driver}.h5', 'r', driver=hdf5_driver) as f:
+                    en_time_hdf5 = time.time()
+                    tmp_load_hdf5.append(en_time_hdf5 - st_time_hdf5)
+                    
+                    ### MANIPULATION ###
+                    def get_all_image_datasets():
+                        image_datasets = []                
+                        def visit_func(name, obj):
+                            if isinstance(obj, h5py.Dataset) and ("type" in obj.attrs) and obj.attrs["type"] == "image" and obj.attrs["label"] == selected_label :
+                                new_obj = np.transpose(obj)   
+                                new_obj = np.square(new_obj)
+                                new_obj = np.exp(new_obj)
+                                new_obj = np.transpose(new_obj)
+                                image_datasets.append(new_obj[:])
+                        f.visititems(visit_func)
+                        return np.array(image_datasets)
+                    start_time_hdf5 = time.time()
+                    image_ds = get_all_image_datasets()
+                    end_time_hdf5 = time.time()
+                    tmp_manipulate_hdf5.append(end_time_hdf5-start_time_hdf5)
+
+            t_load_arrow.append(sum(tmp_load_arrow) / len(tmp_load_arrow))
+            t_manipulate_arrow.append(sum(tmp_manipulate_arrow) / len(tmp_manipulate_arrow))
+
+            t_load_hdf5.append(sum(tmp_load_hdf5) / len(tmp_load_hdf5))
+            t_manipulate_hdf5.append(sum(tmp_manipulate_hdf5) / len(tmp_manipulate_hdf5))
+
+        if plot:
+            plt.title("HDF5 vs Arrow Loading")
+            plt.plot(N, t_load_hdf5, label="hdf5", color='blue')
+
+            # Create the second plot
+            plt.plot(N, t_load_arrow, label="arrow", color='red')
+
+            # Add a legend
+            plt.legend()
+
+            # Add titles and labels
+            plt.xlabel("N (number of samples)")
+            plt.ylabel("t (seconds)")
+
+            # Show the plot
+            plt.savefig(f'{path}_{hdf5_driver}_load.pdf')  # Save as PDF
+            plt.show()
+
+            plt.title("HDF5 vs Arrow Manipulating")
+            plt.plot(N, t_manipulate_hdf5, label="hdf5", color='blue')
+
+            # Create the second plot
+            plt.plot(N, t_manipulate_arrow, label="arrow", color='red')
+
+            # Add a legend
+            plt.legend()
+
+            # Add titles and labels
+            plt.xlabel("N (number of samples)")
+            plt.ylabel("t (seconds)")
+
+            # Show the plot
+            plt.savefig(f'{path}_{hdf5_driver}_manipulate.pdf')  # Save as PDF
+            plt.show()
+
+        if save:
+            np.save(f"{path}_t_load_arrow.npy",t_load_arrow)
+            np.save(f"{path}_t_manipulate_arrow.npy",t_manipulate_arrow)
+            np.save(f"{path}_t_load_hdf5_{hdf5_driver}.npy",t_load_hdf5)
+            np.save(f"{path}_t_manipulate_hdf5_{hdf5_driver}.npy",t_manipulate_hdf5)
+            
+        return t_load_arrow, t_manipulate_arrow , t_load_hdf5 , t_manipulate_hdf5
+    
+    def benchmark_bounding_boxes_conversion(self,
+                                            path : str, 
+                                            N : List[int],
+                                            iterations : int,
+                                            selected_label : int,
+                                            hdf5_driver : str = None,
+                                            plot : bool = False,
+                                            save : bool = False):
+        
+        t_load_hdf5 = []
+        t_manipulate_hdf5 = []
+
+        t_load_arrow = []
+        t_manipulate_arrow = []
+
+        for item in N:
+
+            tmp_load_hdf5 = []
+            tmp_manipulate_hdf5 = []
+
+            tmp_load_arrow = []
+            tmp_manipulate_arrow = []
+
+            
+            for j in tqdm(range(iterations)):
+                ### LOADING ###
+                st_time_arrow = time.time()
+                table = pq.read_table(f'{path}_{item}.parquet')
+                en_time_arrow = time.time()
+                tmp_load_arrow.append(en_time_arrow - st_time_arrow)
+
+                ### MANIPULATION ###
+                start_time_arrow = time.time()
+                
+                oggetti = np.array(table.column("image_feature").chunk(0).values.field("bounding_box_feature").values.field("bbox").to_pylist())
+                formati = np.array(table.column("image_feature").chunk(0).values.field("bounding_box_feature").values.field("format").to_pylist())
+                formati = formati[:,np.newaxis]
+                oggetti = np.hstack((oggetti,formati))
+                inverted_bboxes = np.apply_along_axis(xycenterwh_to_xyminmax, axis=1, arr=oggetti)
+                end_time_arrow = time.time()
+                tmp_manipulate_arrow.append(end_time_arrow-start_time_arrow)
+
+                ### LOADING ###
+                st_time_hdf5 = time.time()
+                with h5py.File(f'{path}_{item}_{hdf5_driver}.h5', 'r', driver=hdf5_driver) as f:
+                    en_time_hdf5 = time.time()
+                    tmp_load_hdf5.append(en_time_hdf5 - st_time_hdf5)
+                    
+                    ### MANIPULATION ###
+                    def get_all_image_datasets():
+                        bbox_datasets = []                
+                        def visit_func(name, obj):
+                            if isinstance(obj, h5py.Dataset) and ("type" in obj.attrs) and obj.attrs["type"] == "bbox" and obj.attrs["format"] == 0:
+                                bbox = obj[:]
+                                x_min = bbox[0] - bbox[2] / 2
+                                y_min = bbox[1] - bbox[3] / 2
+                                x_max = bbox[0] + bbox[2] / 2
+                                y_max = bbox[1] + bbox[3] / 2
+                                bbox_datasets.append[x_min, y_min, x_max, y_max]
+                        f.visititems(visit_func)
+                        return np.array(bbox_datasets)
+                    start_time_hdf5 = time.time()
+                    bbox_ds = get_all_image_datasets()
+                    end_time_hdf5 = time.time()
+                    tmp_manipulate_hdf5.append(end_time_hdf5-start_time_hdf5)
+
+            t_load_arrow.append(sum(tmp_load_arrow) / len(tmp_load_arrow))
+            t_manipulate_arrow.append(sum(tmp_manipulate_arrow) / len(tmp_manipulate_arrow))
+
+            t_load_hdf5.append(sum(tmp_load_hdf5) / len(tmp_load_hdf5))
+            t_manipulate_hdf5.append(sum(tmp_manipulate_hdf5) / len(tmp_manipulate_hdf5))
+
+        if plot:
+            plt.title("HDF5 vs Arrow Loading")
+            plt.plot(N, t_load_hdf5, label="hdf5", color='blue')
+
+            # Create the second plot
+            plt.plot(N, t_load_arrow, label="arrow", color='red')
+
+            # Add a legend
+            plt.legend()
+
+            # Add titles and labels
+            plt.xlabel("N (number of samples)")
+            plt.ylabel("t (seconds)")
+
+            # Show the plot
+            plt.savefig(f'{path}_{hdf5_driver}_load.pdf')  # Save as PDF
+            plt.show()
+
+            plt.title("HDF5 vs Arrow Manipulating")
+            plt.plot(N, t_manipulate_hdf5, label="hdf5", color='blue')
+
+            # Create the second plot
+            plt.plot(N, t_manipulate_arrow, label="arrow", color='red')
+
+            # Add a legend
+            plt.legend()
+
+            # Add titles and labels
+            plt.xlabel("N (number of samples)")
+            plt.ylabel("t (seconds)")
+
+            # Show the plot
+            plt.savefig(f'{path}_{hdf5_driver}_manipulate.pdf')  # Save as PDF
+            plt.show()
+
+        if save:
+            np.save(f"{path}_t_load_arrow.npy",t_load_arrow)
+            np.save(f"{path}_t_manipulate_arrow.npy",t_manipulate_arrow)
+            np.save(f"{path}_t_load_hdf5_{hdf5_driver}.npy",t_load_hdf5)
+            np.save(f"{path}_t_manipulate_hdf5_{hdf5_driver}.npy",t_manipulate_hdf5)
+            
+        return t_load_arrow, t_manipulate_arrow , t_load_hdf5 , t_manipulate_hdf5
 
 
            
--- src/benchmarkers/f3.py
+++ src/benchmarkers/f3.py
@@ -4,13 +4,19 @@ import pyarrow as pa
 import pyarrow.parquet as pq
 import h5py
 import random
-from typing import Tuple
+from typing import Tuple, List
+import time
+from matplotlib import pyplot as plt
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib.colors as mcolors
 
 class F3(SampleGenerator):
 
-    def create_dataset(self, N: int, d: Tuple, D: int, name : str = "outputs/data_hierarchy"):
+    def create_dataset(self, N: int, d: Tuple, D: int, name : str, hdf5_driver : str):
         output = []
-        with h5py.File(name+".h5", 'w', driver="core") as f:
+        with h5py.File(f"{name}_{hdf5_driver}.h5", 'w', driver="core") as f:
             
             for i in range(N):
                 struct_array = []
@@ -50,3 +56,191 @@ class F3(SampleGenerator):
         table = pa.Table.from_pylist(output)
         df = table.to_pandas()
         df.to_parquet(name+".parquet")
+
+    def benchmark_heatmap_column_wise(self,
+                  path : str, 
+                  N : List[int],
+                  d : List[Tuple],
+                  D : int,
+                  iterations : int,
+                  hdf5_driver : str = None,
+                  plot : bool = False,
+                  save : bool = False):
+         
+        t_load_hdf5 = []
+        t_manipulate_hdf5 = []
+
+        t_load_arrow = []
+        t_manipulate_arrow = []
+        
+
+        for item in N:
+
+            tmp_load_res_arrow = []
+            tmp_manipulate_res_arrow = []
+
+            tmp_load_res_hdf5 = []
+            tmp_manipulate_res_hdf5 = []
+
+            for res in d:
+                tmp_load_arrow = []
+                tmp_manipulate_arrow = []
+
+                tmp_load_hdf5 = []
+                tmp_manipulate_hdf5 = []
+
+                for j in tqdm(range(iterations)):
+                    ### LOADING ###
+                    st_time_arrow = time.time()
+                    table = pq.read_table(f'{path}_{item}_{res}_{D}.parquet')
+                    en_time_arrow = time.time()
+                    tmp_load_arrow.append(en_time_arrow - st_time_arrow)
+                    
+                    ### MANIPULATION ###
+                    start_time_arrow = time.time()
+                    oggetti = np.array(table.column("image_feature").chunk(0).values.field("image").to_pylist())
+                    image_numpy = np.frombuffer(oggetti, dtype=np.float64).reshape((-1,) + res)
+                    new_obj = np.transpose(image_numpy,axes=(0,3,2,1))   
+                    new_obj = np.square(new_obj)
+                    new_obj = np.exp(new_obj)
+                    new_obj = np.transpose(new_obj,axes=(0,3,2,1))
+                    end_time_arrow = time.time()
+                    tmp_manipulate_arrow.append(end_time_arrow-start_time_arrow)
+
+                    ### LOADING ###
+                    st_time_hdf5 = time.time()
+                    with h5py.File(f'{path}_{item}_{res}_{D}_{hdf5_driver}.h5', 'r', driver="core") as f:
+                        en_time_hdf5 = time.time()
+                        tmp_load_hdf5.append(en_time_hdf5 - st_time_hdf5)
+
+                        
+                        ### MANIPULATION ###
+                        def get_all_image_datasets():
+                            image_datasets = []                
+                            def visit_func(name, obj):
+                                if isinstance(obj, h5py.Dataset) and obj.attrs["type"] == "image":
+                                    new_obj = np.transpose(obj)   
+                                    new_obj = np.square(new_obj)
+                                    new_obj = np.exp(new_obj)
+                                    new_obj = np.transpose(new_obj)
+                                    image_datasets.append(new_obj[:])
+                        
+                            f.visititems(visit_func)
+                            return np.array(image_datasets)
+                        
+                        start_time_hdf5 = time.time()
+                        image_ds = get_all_image_datasets()
+                        end_time_hdf5 = time.time()
+                        tmp_manipulate_hdf5.append(end_time_hdf5-start_time_hdf5)
+                
+                tmp_load_res_arrow.append(sum(tmp_load_arrow) / len(tmp_load_arrow))
+                tmp_manipulate_res_arrow.append(sum(tmp_manipulate_arrow) / len(tmp_manipulate_arrow))
+
+                tmp_load_res_hdf5.append(sum(tmp_load_hdf5) / len(tmp_load_hdf5))
+                tmp_manipulate_res_hdf5.append(sum(tmp_manipulate_hdf5) / len(tmp_manipulate_hdf5))
+                    
+            t_load_arrow.append(tmp_load_res_arrow)
+            t_manipulate_arrow.append(tmp_manipulate_res_arrow)
+
+            t_load_hdf5.append(tmp_load_res_hdf5)
+            t_manipulate_hdf5.append(tmp_manipulate_res_hdf5)
+
+        if plot:
+            data1 = np.array(t_load_arrow)
+            data2 = np.array(t_load_hdf5)
+            data3 = np.array(np.array(t_load_hdf5)/np.array(t_load_arrow))
+
+            N_label = [str(item) for item in N]
+            d_label = [str(item) for item in d]
+
+            fig, ax = plt.subplots(1, 3, figsize=(14, 6))
+            plt.suptitle("Loading benchmark")
+
+            vmin = min(data1.min(), data2.min())
+            vmax = max(data1.max(), data2.max())
+            norm = mcolors.Normalize(vmin=vmin, vmax=vmax)
+
+            # First heatmap
+            cax1 = ax[0].imshow(data1, cmap='coolwarm', interpolation='gaussian', norm=norm)
+            ax[0].set_title('HDF5')
+            fig.colorbar(cax1, ax=ax[0], orientation='vertical')
+            ax[0].set_xticks(ticks=np.arange(len(d_label)), labels=d_label,rotation=45)
+            ax[0].set_yticks(ticks=np.arange(len(N_label)), labels=N_label)
+
+            # Second heatmap
+            cax2 = ax[1].imshow(data2, cmap='coolwarm', interpolation='gaussian',norm=norm)
+            ax[1].set_title('Arrow')
+            fig.colorbar(cax2, ax=ax[1], orientation='vertical')
+            ax[1].set_xticks(ticks=np.arange(len(d_label)), labels=d_label,rotation=45)
+            ax[1].set_yticks(ticks=np.arange(len(N_label)), labels=N_label)
+
+            # Third heatmap
+            cax3 = ax[2].imshow(data3, cmap='inferno', interpolation='gaussian')
+            ax[2].set_title('HDF5 / Arrrow')
+            fig.colorbar(cax3, ax=ax[2], orientation='vertical')
+            ax[2].set_xticks(ticks=np.arange(len(d_label)), labels=d_label,rotation=45)
+            ax[2].set_yticks(ticks=np.arange(len(N_label)), labels=N_label)
+
+            # Adjust layout to prevent overlap
+            plt.tight_layout()
+
+            # Show the plot
+            plt.savefig(f'{path}_{hdf5_driver}_{D}_loading.pdf')  # Save as PDF
+            plt.show()
+
+##################################################################################################
+##################################################################################################
+##################################################################################################
+
+            data1 = np.array(t_manipulate_arrow)
+            data2 = np.array(t_manipulate_hdf5)
+            data3 = np.array(np.array(t_manipulate_hdf5)/np.array(t_manipulate_arrow))
+
+            N_label = [str(item) for item in N]
+            d_label = [str(item) for item in d]
+
+            fig, ax = plt.subplots(1, 3, figsize=(14, 6))
+            plt.suptitle("Manipulating benchmark")
+
+            vmin = min(data1.min(), data2.min())
+            vmax = max(data1.max(), data2.max())
+            norm = mcolors.Normalize(vmin=vmin, vmax=vmax)
+
+            # First heatmap
+            cax1 = ax[0].imshow(data1, cmap='coolwarm', interpolation='gaussian', norm=norm)
+            ax[0].set_title('HDF5')
+            fig.colorbar(cax1, ax=ax[0], orientation='vertical')
+            ax[0].set_xticks(ticks=np.arange(len(d_label)), labels=d_label,rotation=45)
+            ax[0].set_yticks(ticks=np.arange(len(N_label)), labels=N_label)
+
+            # Second heatmap
+            cax2 = ax[1].imshow(data2, cmap='coolwarm', interpolation='gaussian',norm=norm)
+            ax[1].set_title('Arrow')
+            fig.colorbar(cax2, ax=ax[1], orientation='vertical')
+            ax[1].set_xticks(ticks=np.arange(len(d_label)), labels=d_label,rotation=45)
+            ax[1].set_yticks(ticks=np.arange(len(N_label)), labels=N_label)
+
+            # Third heatmap
+            cax3 = ax[2].imshow(data3, cmap='inferno', interpolation='gaussian')
+            ax[2].set_title('HDF5 / Arrrow')
+            fig.colorbar(cax3, ax=ax[2], orientation='vertical')
+            ax[2].set_xticks(ticks=np.arange(len(d_label)), labels=d_label,rotation=45)
+            ax[2].set_yticks(ticks=np.arange(len(N_label)), labels=N_label)
+
+            # Adjust layout to prevent overlap
+            plt.tight_layout()
+            
+
+            # Show the plot
+            plt.savefig(f'{path}_{hdf5_driver}_{D}_manipulating.pdf')  # Save as PDF
+            plt.show()
+
+
+        if save:
+            np.save(f"{path}_t_load_arrow_{D}.npy",t_load_arrow)
+            np.save(f"{path}_t_manipulate_arrow_{D}.npy",t_manipulate_arrow)
+            np.save(f"{path}_t_load_hdf5_{hdf5_driver}_{D}.npy",t_load_hdf5)
+            np.save(f"{path}_t_manipulate_hdf5_{hdf5_driver}_{D}.npy",t_manipulate_hdf5)
+            
+        return t_load_arrow, t_manipulate_arrow , t_load_hdf5 , t_manipulate_hdf5
+
