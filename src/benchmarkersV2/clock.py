fc3b6d3455622afbbd35d176fe4582470fb69c83
CristianoCarta <cristiano.carta23@gmail.com>
2024-09-04 14:39:58 +0200

    more benchmarks

--- src/benchmarkersV2/clock.py
+++ src/benchmarkersV2/clock.py
@@ -204,5 +204,222 @@ class ClockColumnWise:
         return self
 
 
+class ClockRowWise:
 
-           
+    def benchmark_parquet(self,
+                  path : str, 
+                  N : List[int],
+                  iterations : int,
+                  selected_label : int,
+                  dim : int
+                 ):
+         
+        t_load_arrow = []
+        t_manipulate_arrow = []
+
+        for item in N:
+
+            tmp_load_arrow = []
+            tmp_manipulate_arrow = []
+            
+            for j in tqdm(range(iterations)):
+                ### LOADING ###
+                st_time_arrow = time.time()
+                table = pq.read_table(f'{path}_{item}.parquet')
+                en_time_arrow = time.time()
+                tmp_load_arrow.append(en_time_arrow - st_time_arrow)
+
+                ### MANIPULATION ###
+                start_time_arrow = time.time()
+                manipulated_images = []
+                for k in range(item):
+                    if any( x == selected_label for x in table.column("image_feature").chunk(0)[k][0]["boundingbox_feature"][2]["label_feature"].values.field("label").to_pylist()) :  ## Indice di immagine deve variare nel framework
+                        obj = table.column("image_feature").chunk(0)[k][0]["image"]
+                        image_numpy = np.frombuffer(obj.as_buffer(), dtype=np.float64).reshape(3, dim, dim)
+                        new_obj = np.transpose(image_numpy)   
+                        new_obj = np.square(new_obj)
+                        new_obj = np.exp(new_obj)
+                        new_obj = np.transpose(new_obj)
+                        manipulated_images.append(new_obj)
+                end_time_arrow = time.time()
+                tmp_manipulate_arrow.append(end_time_arrow-start_time_arrow)
+
+            t_load_arrow.append(sum(tmp_load_arrow) / len(tmp_load_arrow))
+            t_manipulate_arrow.append(sum(tmp_manipulate_arrow) / len(tmp_manipulate_arrow))
+
+        self.t_load = t_load_arrow
+        self.t_manipulate = t_manipulate_arrow
+        return self
+        
+    def benchmark_hdf5(self,
+                  path : str, 
+                  N : List[int],
+                  iterations : int,
+                  selected_label : int,
+                  hdf5_driver : str = None
+                  ):
+         
+        t_load_hdf5 = []
+        t_manipulate_hdf5 = []
+
+        for item in N:
+
+            tmp_load_hdf5 = []
+            tmp_manipulate_hdf5 = []
+            
+            for j in tqdm(range(iterations)):
+                
+                ### LOADING ###
+                st_time_hdf5 = time.time()
+                with h5py.File(f'{path}_{item}_{hdf5_driver}.h5', 'r', driver=hdf5_driver) as f:
+                    en_time_hdf5 = time.time()
+                    tmp_load_hdf5.append(en_time_hdf5 - st_time_hdf5)
+                    
+                    ### MANIPULATION ###
+                    image_datasets = []
+                    start_time_hdf5 = time.time()
+                    for i in range(item):
+                        dataset = f.get(f"example_{i}/image_feature/image1/boundingbox_feature/bb3/bbox")
+                        if dataset and ("label" in dataset.attrs) and dataset.attrs["label"] == selected_label:
+                            obj = dataset[:]
+                            new_obj = np.transpose(obj)   
+                            new_obj = np.square(new_obj)
+                            new_obj = np.exp(new_obj)
+                            new_obj = np.transpose(new_obj)
+                            image_datasets.append(new_obj[:])
+                    end_time_hdf5 = time.time()
+                    tmp_manipulate_hdf5.append(end_time_hdf5-start_time_hdf5)
+
+            t_load_hdf5.append(sum(tmp_load_hdf5) / len(tmp_load_hdf5))
+            t_manipulate_hdf5.append(sum(tmp_manipulate_hdf5) / len(tmp_manipulate_hdf5))
+
+        self.t_load = t_load_hdf5
+        self.t_manipulate = t_manipulate_hdf5
+        return self
+        
+    def benchmark_arrow(
+            self,
+            path : str, 
+            N : List[int],
+            iterations : int,
+            dim : int,
+            selected_label : int,
+            memory : bool = False,
+            stream : bool = False):
+        
+        t_load = []
+        t_manipulate = []
+
+        for item in N:
+
+            tmp_load = []
+            tmp_manipulate = []
+            
+            for j in tqdm(range(iterations)):
+
+                
+                if stream and memory:
+                    st_time_load = time.time()
+                    with pa.memory_map(f'{path}_{item}_stream.arrows', 'rb') as source:
+                        table = pa.ipc.open_stream(source).read_all()
+                        en_time_load = time.time()
+                        tmp_load.append(en_time_load - st_time_load)
+
+                    start_time_arrow = time.time()
+                    manipulated_images = []
+                    for k in range(item):
+                        if any( x == selected_label for x in table.column("image_feature").chunk(0)[k][0]["boundingbox_feature"][2]["label_feature"].values.field("label").to_pylist()) :  ## Indice di immagine deve variare nel framework
+                            obj = table.column("image_feature").chunk(0)[k][0]["image"]
+                            image_numpy = np.frombuffer(obj.as_buffer(), dtype=np.float64).reshape(3, dim, dim)
+                            new_obj = np.transpose(image_numpy)   
+                            new_obj = np.square(new_obj)
+                            new_obj = np.exp(new_obj)
+                            new_obj = np.transpose(new_obj)
+                            manipulated_images.append(new_obj)
+                    end_time_arrow = time.time()
+                    tmp_manipulate.append(end_time_arrow-start_time_arrow)
+
+                    #t_load.append(sum(tmp_load) / len(tmp_load))
+                    #_manipulate.append(sum(tmp_manipulate) / len(tmp_manipulate))
+                
+                elif stream and (not memory):
+                    st_time_load = time.time()
+                    with pa.OSFile(f'{path}_{item}_stream.arrows', 'rb') as source:
+                        table = pa.ipc.open_stream(source).read_all()
+                        en_time_load = time.time()
+                        tmp_load.append(en_time_load - st_time_load)
+
+                    start_time_arrow = time.time()
+                    manipulated_images = []
+                    for k in range(item):
+                        if any( x == selected_label for x in table.column("image_feature").chunk(0)[k][0]["boundingbox_feature"][2]["label_feature"].values.field("label").to_pylist()) :  ## Indice di immagine deve variare nel framework
+                            obj = table.column("image_feature").chunk(0)[k][0]["image"]
+                            image_numpy = np.frombuffer(obj.as_buffer(), dtype=np.float64).reshape(3, dim, dim)
+                            new_obj = np.transpose(image_numpy)   
+                            new_obj = np.square(new_obj)
+                            new_obj = np.exp(new_obj)
+                            new_obj = np.transpose(new_obj)
+                            manipulated_images.append(new_obj)
+                    end_time_arrow = time.time()
+                    tmp_manipulate.append(end_time_arrow-start_time_arrow)
+
+                    #t_load.append(sum(tmp_load) / len(tmp_load))
+                    #t_manipulate.append(sum(tmp_manipulate) / len(tmp_manipulate))
+
+                elif (not stream) and memory:
+                    st_time_load = time.time()
+                    with pa.memory_map(f'{path}_{item}_file.arrow', 'rb') as source:
+                        table = pa.ipc.open_file(source).read_all()
+                        en_time_load = time.time()
+                        tmp_load.append(en_time_load - st_time_load)
+
+                    start_time_arrow = time.time()
+                    manipulated_images = []
+                    for k in range(item):
+                        if any( x == selected_label for x in table.column("image_feature").chunk(0)[k][0]["boundingbox_feature"][2]["label_feature"].values.field("label").to_pylist()) :  ## Indice di immagine deve variare nel framework
+                            obj = table.column("image_feature").chunk(0)[k][0]["image"]
+                            image_numpy = np.frombuffer(obj.as_buffer(), dtype=np.float64).reshape(3, dim, dim)
+                            new_obj = np.transpose(image_numpy)   
+                            new_obj = np.square(new_obj)
+                            new_obj = np.exp(new_obj)
+                            new_obj = np.transpose(new_obj)
+                            manipulated_images.append(new_obj)
+                    end_time_arrow = time.time()
+                    tmp_manipulate.append(end_time_arrow-start_time_arrow)
+
+                    #t_load.append(sum(tmp_load) / len(tmp_load))
+                    #t_manipulate.append(sum(tmp_manipulate) / len(tmp_manipulate))
+
+                elif (not stream) and (not memory):
+                    st_time_load = time.time()
+                    with pa.OSFile(f'{path}_{item}_file.arrow', 'rb') as source:
+                        table = pa.ipc.open_file(source).read_all()
+                        en_time_load = time.time()
+                        tmp_load.append(en_time_load - st_time_load)
+
+                    start_time_arrow = time.time()
+                    manipulated_images = []
+                    for k in range(item):
+                        if any( x == selected_label for x in table.column("image_feature").chunk(0)[k][0]["boundingbox_feature"][2]["label_feature"].values.field("label").to_pylist()) :  ## Indice di immagine deve variare nel framework
+                            obj = table.column("image_feature").chunk(0)[k][0]["image"]
+                            image_numpy = np.frombuffer(obj.as_buffer(), dtype=np.float64).reshape(3, dim, dim)
+                            new_obj = np.transpose(image_numpy)   
+                            new_obj = np.square(new_obj)
+                            new_obj = np.exp(new_obj)
+                            new_obj = np.transpose(new_obj)
+                            manipulated_images.append(new_obj)
+                    end_time_arrow = time.time()
+                    tmp_manipulate.append(end_time_arrow-start_time_arrow)
+
+                    #t_load.append(sum(tmp_load) / len(tmp_load))
+                    #t_manipulate.append(sum(tmp_manipulate) / len(tmp_manipulate))
+
+                else:
+                    raise NotImplementedError("something went wrong")
+        
+            t_load.append(sum(tmp_load) / len(tmp_load))
+            t_manipulate.append(sum(tmp_manipulate) / len(tmp_manipulate))
+
+        self.t_load = t_load
+        self.t_manipulate = t_manipulate
+        return self
--- src/benchmarkersV2/generator.py
+++ src/benchmarkersV2/generator.py
@@ -13,80 +13,79 @@ class Generator:
 
 
     def create_dataset(self, N: int, name : str, dim : int):
-            output = []
-            for i in range(N):
-                text1 = "Foto di una nebulosa"
-                text2 = "Foto infrarossi di una nebulosa"
-                image1 = np.random.rand(3,dim,dim)
-                image2 = np.random.rand(3,dim,dim)
-                image3 = np.random.rand(3,dim,dim)
-                shape = (3,dim,dim)
-    
-                
-                bb1 = np.random.rand(4)
-                bb2 = np.random.rand(4)
-                bb3 = np.random.rand(4)
-                bb4 = np.random.rand(4)
-                bb5 = np.random.rand(4)
-                image4 = np.random.rand(3,dim,dim)
-                label1 = np.random.randint(100,size=1)[0]
-
-                im1b = image1.tobytes()
-                im1b = image1.tobytes()
-                im1b = image1.tobytes()
-                im1b = image1.tobytes()
-
-                # print(np.frombuffer(im1b.as_buffer(), dtype=np.float32).shape)
-
-                sample = {
-                    "image_feature": [
-                        {
-                            "image" : image1.tobytes(),
-                            "shape" : shape,
-                            "boundingbox_feature":[{
-                                "bbox" : bb1,
-                                "image_1_feature": [{
-                                    "image" : image4.tobytes(),
-                                    "shape" : shape,
-                                }]
-                            },
-                            {
-                                "bbox" : bb2
-                            },
-                            {
-                                "bbox" : bb3,
-                                "label_feature": [{
-                                    "label" : label1
-                                }]
-                            }]
-                        },
-                        {
-                            "image" : image2.tobytes(),
-                            "shape" : shape,
-                            "boundingbox_feature":[{
-                                "bbox" : bb4
-                            },{
-                                "bbox" : bb5
-                            }]
-                        },
-                        {
-                            "image" : image3.tobytes(),
-                            "shape" : shape,
-                            "text_1_feature":[
-                                {
-                                    "text": text2
-                                }
-                            ]
-                        }
-                    ],
-                    "text_feature" : [
-                        {
-                            "text": text1
-                        }
-                    ],
-
-                }
-                output.append(sample)
+            #output = []
+            #for i in range(N):
+            text1 = "Foto di una nebulosa"
+            text2 = "Foto infrarossi di una nebulosa"
+            image1 = np.random.rand(3,dim,dim)
+            image2 = np.random.rand(3,dim,dim)
+            image3 = np.random.rand(3,dim,dim)
+            shape = (3,dim,dim)
+    #
+            #    
+            bb1 = np.random.rand(4)
+            bb2 = np.random.rand(4)
+            bb3 = np.random.rand(4)
+            bb4 = np.random.rand(4)
+            bb5 = np.random.rand(4)
+            image4 = np.random.rand(3,dim,dim)
+            label1 = np.random.randint(100,size=1)[0]
+            #im1b = image1.tobytes()
+            #im1b = image1.tobytes()
+            #im1b = image1.tobytes()
+            #im1b = image1.tobytes()
+#
+            #    # print(np.frombuffer(im1b.as_buffer(), dtype=np.float32).shape)
+#
+            #    sample = {
+            #        "image_feature": [
+            #            {
+            #                "image" : image1.tobytes(),
+            #                "shape" : shape,
+            #                "boundingbox_feature":[{
+            #                    "bbox" : bb1,
+            #                    "image_1_feature": [{
+            #                        "image" : image4.tobytes(),
+            #                        "shape" : shape,
+            #                    }]
+            #                },
+            #                {
+            #                    "bbox" : bb2
+            #                },
+            #                {
+            #                    "bbox" : bb3,
+            #                    "label_feature": [{
+            #                        "label" : label1
+            #                    }]
+            #                }]
+            #            },
+            #            {
+            #                "image" : image2.tobytes(),
+            #                "shape" : shape,
+            #                "boundingbox_feature":[{
+            #                    "bbox" : bb4
+            #                },{
+            #                    "bbox" : bb5
+            #                }]
+            #            },
+            #            {
+            #                "image" : image3.tobytes(),
+            #                "shape" : shape,
+            #                "text_1_feature":[
+            #                    {
+            #                        "text": text2
+            #                    }
+            #                ]
+            #            }
+            #        ],
+            #        "text_feature" : [
+            #            {
+            #                "text": text1
+            #            }
+            #        ],
+#
+            #    }
+            #    output.append(sample)
 
             with h5py.File(f"{name}_core.h5", 'w') as f:
             # Create image_feature group
@@ -126,7 +125,8 @@ class Generator:
                     bb2.create_dataset('bbox', data=bb2)  # Replace with actual bbox
                     
                     bb3 = boundingbox_feature1.create_group('bb3')
-                    bb3.create_dataset('bbox', data=bb3)  # Replace with actual bbox
+                    bb3ds = bb3.create_dataset('bbox', data=bb3)  # Replace with actual bbox
+                    bb3ds.attrs["label"] = label1
                     
                     label_feature = bb3.create_group('label_feature')
                     label_feature.create_dataset('label', data=label1)
@@ -154,8 +154,8 @@ class Generator:
                     text_feature = example.create_group('text_feature')
                     text_feature.create_dataset('text', data=text1)
 
-            self.table = pa.Table.from_pylist(output)
-            pq.write_table(self.table, name+".parquet")
+            #self.table = pa.Table.from_pylist(output)
+            #pq.write_table(self.table, name+".parquet")
             shutil.copy(f"{name}_core.h5", f"{name}_sec2.h5")
 
             
