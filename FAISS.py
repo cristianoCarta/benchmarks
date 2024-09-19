8b92b749bceb6fdb299361ce6d08cd34d4cd8c5b
CristianoCarta <cristiano.carta23@gmail.com>
2024-09-19 17:25:31 +0200

    normalizzazione

--- FAISS.py
+++ FAISS.py
@@ -50,26 +50,43 @@ def knn(root_path_animal : str,
     emb_species = np.array(ff.get_feature(table_2,emb_path_species).to_pylist())
     class_animals = ff.get_feature(table,class_path_animals).to_pylist()
 
+    emb_animals_norm = []
+    emb_species_norm = []
+
+    for emb_an in emb_animals:
+        norm = np.linalg.norm(emb_an,ord=2)
+        emb_animals_norm.append(emb_an/norm)
+    
+    for emb_sp in emb_species:
+        norm = np.linalg.norm(emb_sp,ord=2)
+        emb_species_norm.append(emb_sp/norm)
+
+    emb_animals_norm = np.array(emb_animals_norm)
+    emb_species_norm = np.array(emb_species_norm)
+          
+
     image_species = np.array(ff.get_feature(table_2,["image_feature","image"]))
 
     d = len(emb_animals[0])
-    X = emb_animals.astype("float32")
-    xq = emb_species.astype("float32")
+    X = emb_animals_norm.astype("float32")
+    xq = emb_species_norm.astype("float32")
     index = faiss.IndexFlatL2(d) 
     index.add(X) 
     D, I = index.search(xq, k)
 
-    print(I)
-    print(D)
-
     output = []
     counter = 0
+
+    I_t = []
+    D_t = []
 
     for iv,dv in zip(I,D):
         iv = np.array(iv)
         dv = np.array(dv)
         iv = [iv[i] if dv[i]<thr else None for i in range(len(dv))]
         dv = [dv[i] if dv[i]<thr else None for i in range(len(dv))]
+        I_t.append(iv)
+        D_t.append(dv)
 
         found_number_of_neighbours = len([x for x in iv if x is not None])
         new_candidate_classes = [class_animals[item] for item in iv if item]
@@ -103,8 +120,8 @@ def knn(root_path_animal : str,
     new_table = pa.Table.from_pylist(output)
     ff.partition_dataset(new_table,root_path_to_merge)
 
-    np.save(r"C:\Users\Cristiano Lavoro\Desktop\benchmarks\knn_outputs\to_merge\N.npy",I)
-    np.save(r"C:\Users\Cristiano Lavoro\Desktop\benchmarks\knn_outputs\to_merge\D.npy",D)
+    np.save(r"C:\Users\Cristiano Lavoro\Desktop\benchmarks\knn_outputs\resnet\to_merge\I.npy",I_t)
+    np.save(r"C:\Users\Cristiano Lavoro\Desktop\benchmarks\knn_outputs\resnet\to_merge\D.npy",D_t)
 
     return pa.concat_tables([table,new_table])
 
@@ -119,5 +136,5 @@ merged_table = knn(root_path_animal,
         ["image_feature","embedding_feature","vector"],
         ["image_feature","class_feature","label"],
         6,
-        35)
+        15)
 
